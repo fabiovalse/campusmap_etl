1ebad55a7d5c9ced88bad36348001c229a6abb5f
fabiovalse <fabiovalse@gmail.com>
2017-09-05 17:32:34 +0200

    ArangoDB loader

---
+++ load/arango.py
@@ -0,0 +1,14 @@
+import sys
+import json
+from pyArango.connection import *
+
+json_data = json.loads(sys.stdin.read())
+
+conn = Connection(username="root", password="c25a2017")
+
+db = conn["campusmap"]
+collection = db["Prova"]
+
+d = collection.createDocument()
+d.set(json_data)
+d.save()
