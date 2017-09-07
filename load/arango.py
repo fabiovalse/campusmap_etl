391d46d2e67bf9edfd97d1ff26073768e4c8d215
nitaku <matteo.abrate@gmail.com>
2017-09-07 15:48:42 +0200

    Command line args in Arango loader

--- load/arango.py
+++ load/arango.py
@@ -1,14 +1,22 @@
 import sys
 import json
+import argparse
 import pyArango
 from pyArango.connection import *
 
+parser = argparse.ArgumentParser(description='Loads a JSON document into ArangoDB.')
+parser.add_argument('--username', metavar='u', type=str)
+parser.add_argument('--password', metavar='p', type=str)
+parser.add_argument('--db', metavar='d', type=str)
+parser.add_argument('--coll', metavar='c', type=str)
+args = parser.parse_args()
+
 json_data = json.loads(sys.stdin.read())
 
-conn = Connection(username="root", password="c25a2017")
+conn = Connection(username=args.username, password=args.password)
 
-db = conn["campusmap"]
-collection = db["Prova"]
+db = conn[args.db]
+collection = db[args.coll]
 
 d = collection.createDocument()
 d.set(json_data)
