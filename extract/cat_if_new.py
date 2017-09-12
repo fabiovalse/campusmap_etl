2589b1a3bc88237b9fd6cda08c6f02555982cd9f
nitaku <matteo.abrate@gmail.com>
2017-09-12 15:05:25 +0200

    60 secs -> 30 secs

--- extract/cat_if_new.py
+++ extract/cat_if_new.py
@@ -3,7 +3,7 @@ import argparse
 import time
 import datetime
 
-parser = argparse.ArgumentParser(description='Outputs a file to STDOUT if it was modified less than a minute ago.')
+parser = argparse.ArgumentParser(description='Outputs a file to STDOUT if it was modified less than 30 sec ago.')
 parser.add_argument('--filename', metavar='f', type=str)
 args = parser.parse_args()
 
@@ -11,7 +11,7 @@ now = time.time()
 mtime = os.stat(args.filename).st_mtime
 
 # this makes the script fail if the file is old
-assert now-mtime < 60
+assert now-mtime < 30
 
 with open(args.filename, 'r') as f:
     print(f.read())
